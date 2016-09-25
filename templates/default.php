48b562626b7fbdab91ea32c302261981894e3ec0
Mark <m.l.bentley81@gmail.com>
2016-09-25 19:07:09 +0100

    Removed head tag from template

--- templates/default.php
+++ templates/default.php
@@ -5,7 +5,7 @@
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 
  
- <head>
+ 
  <meta charset="UTF-8" />
    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,7 +18,7 @@
    <link rel="stylesheet" href="<?php echo DIR_ASSET; ?>/css/normalize.css" />
    <!-- call css -->
    <link rel="stylesheet" href="<?php echo DIR_ASSET; ?>/css/style.css" />
- </head>
+
  <body>
  <!-- container -->
   
